121022e92a5fe5e7490d6c2689145d61652697d6
kiki <k.kazaneko@gmail.com>
2022-08-25 21:20:02 +0900

    create Advice component

---
+++ src/components/Advice.js
@@ -0,0 +1,56 @@
+import { Box, Button, ButtonGroup } from "@chakra-ui/react";
+import "../Advice.css";
+import { getActivity, getBuzzword, getAdvice, getJoke } from "../utils/getAPI";
+
+export default function Advice({ currentText, setText }) {
+  function jokeOrAdviceText() {
+    const num = Math.floor(Math.random() * 8);
+    if (num === 3) {
+      setText(getJoke());
+    } else {
+      getAdvice().then((res) => setText(res.slip.advice));
+    }
+  }
+
+  function emptyText() {
+    getBuzzword().then((res) => setText(res.phrase));
+  }
+  return (
+    <>
+      <section>
+        {currentText.length === 0 ? (
+          <h1 className="display-advice">
+            The Word is what you want in your life
+          </h1>
+        ) : (
+          <h1 className="display-advice">{currentText}</h1>
+        )}
+        <div>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            width="100%"
+          >
+            <ButtonGroup gap="4">
+              <Button
+                colorScheme="telegram"
+                variant="solid"
+                onClick={() => jokeOrAdviceText()}
+              >
+                Get An Advice
+              </Button>
+              <Button
+                colorScheme="teal"
+                variant="solid"
+                onClick={() => emptyText()}
+              >
+                Get An Empty Phrase
+              </Button>
+            </ButtonGroup>
+          </Box>
+        </div>
+      </section>
+    </>
+  );
+}
